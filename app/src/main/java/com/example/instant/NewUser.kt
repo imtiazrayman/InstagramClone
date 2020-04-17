cd10fb40bf92b4db1303b6a91f818b6a95c2e0ab
Imtiaz Rayman <raymanimtiaz@gmail.com>
2020-04-17 00:27:59 -0400

    New User activity

---
+++ app/src/main/java/com/example/instant/NewUser.kt
@@ -0,0 +1,12 @@
+package com.example.instant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewUser : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_user)
+    }
+}
