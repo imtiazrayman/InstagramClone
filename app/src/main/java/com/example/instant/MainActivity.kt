b7ee07a8d0b127b1c6b1d1a8618506c7460edf27
Imtiaz Rayman <raymanimtiaz@gmail.com>
2020-04-17 02:28:29 -0400

    Updates

--- app/src/main/java/com/example/instant/MainActivity.kt
+++ app/src/main/java/com/example/instant/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.instant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    fun signup(view: View) {
+        // this button will take you to the sign in page
+    }
+
+    fun login(view: View) {
+        //this button will take you into your profile page.
+        val myIntent= Intent(this, ProfileActivity::class.java)
+        startActivity(myIntent)
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left); // slide out to next activity animation
     }
 }
